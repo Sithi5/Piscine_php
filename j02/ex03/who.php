837daad7474545df8c644c5cea8c6d4e1aa8b8b7
mabouce <mabouce@student.42.fr>
2019-03-25 14:44:26 +0100

    finish who

---
+++ j02/ex03/who.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?
+$template = "A256user/A4id/A32ttyname/ipid/itype/lloginsec/A256host/A64pad";
+$input = file_get_contents("/var/run/utmpx"); 
+while ($input != "")
+{
+	date_default_timezone_set("Europe/Paris");
+	$result = unpack($template, $input);
+	if ($result['type'] == 7)
+	{
+		echo $result['user'] . "  ";
+		echo $result['ttyname'] . "  ";
+		echo strftime("%b %e %H:%M", $result['loginsec']) . "\n";
+	}
+	$input = substr($input, 628);
+}
+?>
