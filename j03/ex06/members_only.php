1e7381c7c7d8d332e58f0736cc7b9a58c4134ed3
mabouce <mabouce@student.42.fr>
2019-03-28 18:15:54 +0100

    adding \'

--- j03/ex06/members_only.php
+++ j03/ex06/members_only.php
@@ -4,7 +4,7 @@ $img = "../img/42.png";
 $users = array('zaz' => 'jaimelespetitsponeys');
 if ($_SERVER['PHP_AUTH_USER'] != "zaz" || $_SERVER['PHP_AUTH_PW'] != 'jaimelespetitsponeys')
 {
-	header('WWW-Authenticate: Basic realm="Espace membres"');
+	header('WWW-Authenticate: Basic realm=\'\'Espace membres\'\'');
 	header('HTTP/1.0 401 Unauthorized');
 	header('date: ' . date('D, d M Y H:i:s T'));
 	echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>" . "\n";
